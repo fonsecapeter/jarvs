f0dbeddc7a27ea87d6f50ad69c63622970d5ed42
Peter Fonseca <peter.nfonseca@gmail.com>
2016-02-15 20:05:58 -0800

    object orienting... starting with just test of functional check button with dynamic title > can apply to dynamic ui'ing altogether

--- gui.py
+++ gui.py
@@ -5,6 +5,33 @@ import tkMessageBox
 import os
 import subprocess
 
+class Jarvs(Frame):
+
+	def __init__(self, parent):
+		Frame.__init__(self, parent)
+
+		self.parent = parent
+
+		self.init_ui()
+
+	def init_ui(self):
+
+		self.parent.title("Jarvs Gui")
+
+		self.pack(fill=BOTH, expand=True)
+		self.var = BooleanVar()
+
+		# test dynamic check button
+		cb = Checkbutton(self, text="show title", variable=self.var, command=self.toggle_title)
+		cb.select()
+		cb.grid(row=3, column=0, columnspan=2, padx=2, pady=2)
+
+	def toggle_title(self):
+		if self.var.get() == True:
+			self.master.title("Jarvs Gui")
+		else:
+			self.master.title("")
+
 # functionality
 
 def do_nothing():
@@ -32,45 +59,49 @@ def report():
 
 # gui
 
-root = Tk()
-root.wm_title("Jarvs Gui")
+def main():
+	root = Tk()
+	##root.wm_title("Jarvs Gui")
 
-# main men with dropdowns
+	# main men with dropdowns
 
-main_menu = Menu(root, bd=0, activeborderwidth=0)
-root.config(menu=main_menu) # tkinter does everything!
+	main_menu = Menu(root, bd=0, activeborderwidth=0)
+	root.config(menu=main_menu) # tkinter does everything!
 
-file_menu = Menu(main_menu, bd=0, activeborderwidth=0)
-main_menu.add_cascade(label="file", menu=file_menu)
-file_menu.add_command(label="Quick visual", command=vis)
-file_menu.add_command(label="Full report", command=report)
-file_menu.add_separator()
-file_menu.add_command(label="Exit", command=quit)
+	file_menu = Menu(main_menu, bd=0, activeborderwidth=0)
+	main_menu.add_cascade(label="file", menu=file_menu)
+	file_menu.add_command(label="Quick visual", command=vis)
+	file_menu.add_command(label="Full report", command=report)
+	file_menu.add_separator()
+	file_menu.add_command(label="Exit", command=quit)
 
-edit_menu = Menu(main_menu, bd=0, activeborderwidth=0)
-main_menu.add_cascade(label="edit", menu=edit_menu)
-edit_menu.add_command(label="Redo", command=do_nothing)
+	edit_menu = Menu(main_menu, bd=0, activeborderwidth=0)
+	main_menu.add_cascade(label="edit", menu=edit_menu)
+	edit_menu.add_command(label="Redo", command=do_nothing)
 
-# form
-form = Frame(root, bg="white")
+	# form
+	form = Frame(root, bg="white")
 
-label_1 = Label(form, text="Hi, Peter,", anchor=W, bg="white")
-label_2 = Label(form, text="How many I assist you?", anchor=W, bg="white")
-entry_1 = Entry(form, bd=0)
+	label_1 = Label(form, text="Hi, Peter,", anchor=W, bg="white")
+	label_2 = Label(form, text="How many I assist you?", anchor=W, bg="white")
+	entry_1 = Entry(form, bd=0)
 
-label_1.grid(row=0, column=0, sticky=W)
-label_2.grid(row=1, column=0, sticky=W)
-entry_1.grid(row=2, column=0, padx=2, pady=2)
+	label_1.grid(row=0, column=0, sticky=W)
+	label_2.grid(row=1, column=0, sticky=W)
+	entry_1.grid(row=2, column=0, padx=2, pady=2)
 
-# functional button
-button_1 = Button(form, text="Enter", bd=0, bg="white")
-button_1.bind("<Button-1>", do_not_do_something) # lclick = <Button-1>, mclick = <Button-2>, lclick = <Button-3>
-button_1.grid(row=3, column=0, padx=2, pady=2)
+	# functional button
+	button_1 = Button(form, text="Enter", bd=0, bg="white")
+	button_1.bind("<Button-1>", do_not_do_something) # lclick = <Button-1>, mclick = <Button-2>, lclick = <Button-3>
+	button_1.grid(row=3, column=0, padx=2, pady=2)
 
-form.pack(fill=BOTH)
+	form.pack(fill=BOTH)
 
-# status bar
-##status = Label(root, text="Preparing to do nothing", bg="white", anchor=W) # anchor is text align, N E S W
-##status.pack(side=BOTTOM, fill=X)
+	# status bar
+	##status = Label(root, text="Preparing to do nothing", bg="white", anchor=W) # anchor is text align, N E S W
+	##status.pack(side=BOTTOM, fill=X)
+	app = Jarvs(root)
+	root.mainloop()
 
-root.mainloop()
+if __name__ == '__main__':
+	main()
