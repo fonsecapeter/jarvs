37383847785b4e24aa077343fbcfc00486bca760
Peter Fonseca <peter.fonseca@ucsf.edu>
2016-03-10 11:24:40 -0800

    visualization now overrides auto-ytick spacing when < 5 to keep at whole numbers - no such thing as 1.5 RVSs

--- app/rvs/RVS_vis.py
+++ app/rvs/RVS_vis.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import pandas as pd
 import matplotlib.pyplot as plt
+from matplotlib.ticker import MultipleLocator
 import matplotlib
 matplotlib.style.use('ggplot')
 
@@ -15,8 +16,12 @@ df_vis = df[cols_to_keep].tail(12)
 att_call = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
 att_labels = df_vis.att
 
-
 df_vis.plot(kind='bar', stacked=True, color=['#666666', '#C94949'])
+
+# override auto ytick at threshold (found to be when max < 5) to keep ticks at whole numbers 
+if max(df['num_rvs'].tail(12)) < 5:
+	plt.gca().yaxis.set_major_locator(MultipleLocator(1.0))
+
 plt.xticks(att_call, att_labels, rotation=45)
 plt.xlabel("Attending")
 plt.ylabel("Count")
