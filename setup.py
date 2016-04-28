5af19ef5aececfc89a74cfcc2870420d388c9122
Peter Fonseca <peter.nfonseca@gmail.com>
2016-04-27 17:32:00 -0700

    commit before release

--- setup.py
+++ setup.py
@@ -136,7 +136,7 @@ class InstallAndUpdateDataDirectory(DistUtilsExtra.auto.install_auto):
 
 DistUtilsExtra.auto.setup(
     name='jarvs',
-    version='16.04.47',
+    version='16.04.48',
     license='GPL-3',
     author='Peter',
     author_email='peter.nfonseca@gmail.com',
